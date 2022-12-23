f995c10e0fdbeb651941898b48d5aeeda9e31572
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2022-12-23 19:26:12 +0530

    Update admin_check.py

--- ForceSubscribeBot/admin_check.py
+++ ForceSubscribeBot/admin_check.py
@@ -3,19 +3,19 @@ async def admin_check(bot, msg, user_id=None, callback_query=None):
         user_id = msg.from_user.id
     bot_id = (await bot.get_me()).id
     if msg.chat.type not in ["supergroup", "group"]:
-        await msg.reply("This command can only be used in Groups !", quote=True)
+        await msg.reply("<b>This Command Can Only be Used in Groups !</b>", quote=True)
         return False
     chat_member = await msg.chat.get_member(user_id)
     bot_chat_member = await msg.chat.get_member(bot_id)
     if bot_chat_member.status != "administrator":
-        text = "Please make me admin and then try again !"
+        text = "<b>Please make me Admin and then try Again !</b>"
         if callback_query:
             await callback_query.answer(text, show_alert=True)
         else:
             await msg.reply(text)
         return False
     if chat_member.status not in ("creator", "administrator"):
-        text = "This command is only for admins !"
+        text = "<b>This Command is only for Admins !</b>"
         if callback_query:
             await callback_query.answer(text, show_alert=True)
         else:
